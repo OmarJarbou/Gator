c15574b8632238e9b7530c2711c5ad3b2d907f74
Omar Jarbou <omarjarbou2019@gmail.com>
2025-08-09 16:23:30 +0300

    docs: add clarification comment in middlewareLoggedIn function
    - Added a comment to explain the purpose of returning a function instead of an error in the middlewareLoggedIn function, emphasizing the intention to delay handler execution.

--- middleware-logged-in.go
+++ middleware-logged-in.go
@@ -7,6 +7,8 @@ import (
 	"github.com/OmarJarbou/Gator/internal/database"
 )
 
+// we are returning a function instead of just an error because:
+// we don't want to execute the handler immediately, we want to return a function that can be executed later
 func middlewareLoggedIn(handler func(s *state, cmd command, user database.User) error) func(*state, command) error {
 	return func(s *state, cmd command) error {
 		currentUserName := s.Config.CurrentUserName
